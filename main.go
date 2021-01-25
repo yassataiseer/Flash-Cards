15f02ca4d789634c9c6fd134c97dea21b8d241ba
yassataiseer <1taiseeryas@hdsb.ca>
2021-01-25 09:30:07 -0500

    start mysql db connection

---
+++ main.go
@@ -0,0 +1,17 @@
+package main
+
+import(
+	"database/sql"
+	"fmt"
+	_ "github.com/go-sql-driver/mysql"
+)
+
+func main(){
+	fmt.Println("Starting server")
+	db,err := sql.Open("mysql","root:root@tcp(127.0.0.1:3306)/flashcarddb")
+	if err != nil {
+		panic(err)
+	}
+	defer db.Close()
+	fmt.Println("Connected!")
+}
