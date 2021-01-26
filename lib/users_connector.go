16ba7b38118f25edec92145b323de21695a5d04d
yassataiseer <1taiseeryas@hdsb.ca>
2021-01-26 17:56:08 -0500

    commit on main

--- lib/users_connector.go
+++ lib/users_connector.go
@@ -1,7 +1,22 @@
 package lib
 
-import( "fmt")
+import(
+	"database/sql"
+	"fmt"
+	_ "github.com/go-sql-driver/mysql"
+)
 
-func Demo( x string) {
-    fmt.Println(x)
+func add_user( Username string,  password string){
+	fmt.Println("Starting server")
+	db,err := sql.Open("mysql","root:root@tcp(127.0.0.1:3306)/flashcarddb")
+	if err != nil {
+		panic(err)
+	}
+	
+	defer db.Close()
+	add,err := db.Query("INSERT INTO Users (Username,Password) VALUES (?,?)", (Username),(password))
+	if err != nil {
+		panic(err)
+    }
+    fmt.Println(add)
 }
