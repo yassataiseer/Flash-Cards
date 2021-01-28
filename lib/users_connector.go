abd76270f6ddeb844b5d289a9bdaff5758d97f5e
yassataiseer <1taiseeryas@hdsb.ca>
2021-01-28 11:01:21 -0500

    made test case

--- lib/users_connector.go
+++ lib/users_connector.go
@@ -6,19 +6,29 @@ import(
 	_ "github.com/go-sql-driver/mysql"
 )
 
-func add_user( Username string,  password string){
+func add_user( Username string,  password string) bool{
+	fmt.Println("Starting server")
+	db,err := sql.Open("mysql","root:root@tcp(127.0.0.1:3306)/flashcarddb")
+	if err != nil {
+		panic(err)
+	}
+
+	add,err := db.Query("INSERT INTO Users (Username,Password) VALUES (?,?)", (Username),(password))
+	if err != nil {
+        panic(err)
+        return false
+    }
+    fmt.Println(add)
+    defer db.Close()
+    return true 
+}
+func grab_user(){
 	fmt.Println("Starting server")
 	db,err := sql.Open("mysql","root:root@tcp(127.0.0.1:3306)/flashcarddb")
 	if err != nil {
 		panic(err)
 	}
 	defer db.Close()
-	add,err := db.Query("INSERT INTO Users (Username,Password) VALUES (?,?)", (Username),(password))
-	if err != nil {
-		panic(err)
-    }
-    fmt.Println(add)
 }
 func main(){
-add_user("Hareem Taiseer", "Hareem Taiseer")
 }
