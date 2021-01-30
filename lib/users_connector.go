9c5b5b3000a30e5354444bab1ef52f9b5d2c47b8
yassataiseer <1taiseeryas@hdsb.ca>
2021-01-30 11:19:00 -0500

    git add .

--- lib/users_connector.go
+++ lib/users_connector.go
@@ -42,8 +42,22 @@ func sign_user_in( user string, password string) bool{
     return exists
 }
 //Checks to see if user already exists
-func existing_user(){}
+func existing_user(user string) bool{
+    fmt.Println("Starting server")
+	db,err := sql.Open("mysql","root:root@tcp(127.0.0.1:3306)/flashcarddb")
+	if err != nil {panic(err)}
+    var exists bool
+    var query string
+    query = fmt.Sprintf("SELECT EXISTS(SELECT Username FROM Users WHERE Username = '%s')", (user))
+    fmt.Println(query)
+    _ = db.QueryRow(query).Scan(&exists)
+    if err != nil && err != sql.ErrNoRows {
+        panic(err)
+    }
+    defer db.Close()
+    return exists
+}
 
 func main(){
-//fmt.Println(check_if_user_exist("Yassa Taiseer","yassa123"))
+//fmt.Println(existing_user("Yassa Taiseer"))
 }
