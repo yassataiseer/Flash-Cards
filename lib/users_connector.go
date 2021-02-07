cc64eac0542891c47e5fb40b688e6f52374810ba
Yassa Taiseer <yassataiseer@gmail.com>
2021-02-07 02:19:58 -0500

    cleaned code

--- lib/users_connector.go
+++ lib/users_connector.go
@@ -6,9 +6,11 @@ import(
 	_ "github.com/go-sql-driver/mysql"
 )
 type user_data struct {
+//User data struct that acts like a template
+//This template is the used to add all user-name data
+// This is used in the function: Grab_user_data
     Username1 ,Password1 string
 }
-
 
 //Adds user+password into Users table
 func Add_user( Username string,  password string) bool{
@@ -17,8 +19,9 @@ func Add_user( Username string,  password string) bool{
 	if err != nil {
 		panic(err)
 	}
+	add,err := db.Query("INSERT INTO Users (Username,Password) VALUES (?,?)", (Username),(password)) 
+    //Takes username and passwords and adds them to the db
 
-	add,err := db.Query("INSERT INTO Users (Username,Password) VALUES (?,?)", (Username),(password))
 	if err != nil {
         panic(err)
         return false
@@ -32,33 +35,36 @@ func Sign_user_in( user string, password string) bool{
 	fmt.Println("Starting server")
 	db,err := sql.Open("mysql","root:new_password@tcp(127.0.0.1:3306)/flashcarddb")
 	if err != nil {panic(err)}
-    var exists bool
+    var exists bool // Boolean that will hold the value of whether a user exists
     var query string
     query = fmt.Sprintf("SELECT EXISTS(SELECT Username FROM Users WHERE Username = '%s' AND Password = '%s')", (user),(password))
     fmt.Println(query)
-    row := db.QueryRow(query).Scan(&exists)
-	if err != nil && err != sql.ErrNoRows {
+    row := db.QueryRow(query).Scan(&exists)//Scans the Selected rows to see if user exists
+    //This value is then filled in
+    if err != nil && err != sql.ErrNoRows {
         panic(err)
     }
     fmt.Println(row)
     defer db.Close()
-    return exists
+    return exists//returns the boolean
 }
 //Checks to see if user already exists
 func Existing_user(user string) bool{
     fmt.Println("Starting server")
 	db,err := sql.Open("mysql","root:new_password@tcp(127.0.0.1:3306)/flashcarddb")
 	if err != nil {panic(err)}
-    var exists bool
+    var exists bool //Boolean that will hold the value of whether a user exists when scanned
     var query string
     query = fmt.Sprintf("SELECT EXISTS(SELECT Username FROM Users WHERE Username = '%s')", (user))
+    //checks to see if user is in Users table and in the Username Column
+    //The scanned results are kept in the exists boolean
     fmt.Println(query)
     _ = db.QueryRow(query).Scan(&exists)
     if err != nil && err != sql.ErrNoRows {
         panic(err)
     }
     defer db.Close()
-    return exists
+    return exists//returns the boolean
 }
 
 func Grab_user_data() []user_data{
